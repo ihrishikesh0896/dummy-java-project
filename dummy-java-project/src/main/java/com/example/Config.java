bacf35eb5ff08466b301e19c6b4ba9cfa92c2d69
procrastinator <42376445+ihrishikesh0896@users.noreply.github.com>
2024-03-31 14:08:57 +0530

    Replace secrets with placeholder values

--- dummy-java-project/src/main/java/com/example/Config.java
+++ dummy-java-project/src/main/java/com/example/Config.java
@@ -1,6 +1,6 @@
 package com.example;
 
 public class Config {
-    public String apiKey = "12345-abcde-fake-api-key";
+    public String apiKey = "uuid_fb23032be0b844d8a353489451d2486a";
     public String dbPassword = "s3cr3tP@ssw0rd";
 }
